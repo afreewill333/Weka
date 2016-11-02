8115fb2c918af4e1b1506deebffb651c4a834777
John Green <afreewill.me@gmail.com>
2016-11-02 08:50:41 +0800

    Add files via upload

--- WekaApriori.java
+++ WekaApriori.java
@@ -2,6 +2,10 @@ import java.util.*;
 import java.io.*;
 
 import weka.associations.Apriori;
+import weka.associations.AprioriItemSet;
+import weka.associations.AssociationRule;
+import weka.associations.AssociationRules;
+import weka.associations.Item;
 import weka.core.*;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayes;
@@ -46,11 +50,11 @@ public class WekaApriori {
 		 inst.setValue(featureVector.get(2), "positive");
 		 // add the instance
 		 trainingSet.add(inst);
-		 inst.setValue(featureVector.get(0), 21);
+		 inst.setValue(featureVector.get(0), 29);
 		 inst.setValue(featureVector.get(1), "gray");
 		 inst.setValue(featureVector.get(2), "positive");
 		 trainingSet.add(inst);
-		 inst.setValue(featureVector.get(0), 20);
+		 inst.setValue(featureVector.get(0), 19);
 		 inst.setValue(featureVector.get(1), "gray");
 		 inst.setValue(featureVector.get(2), "positive");
 		 trainingSet.add(inst);
@@ -66,17 +70,37 @@ public class WekaApriori {
 		 Apriori apriori = new Apriori();
 		 apriori.buildAssociations(trainingSet);
 		 
+		 AprioriItemSet set = new AprioriItemSet(0);
+		 
 		 List[] listArray = apriori.getAllTheRules();
-		 for(List list: listArray){
-			 for(Object o: list){
-				 System.out.print(o+"\t");
+		 AssociationRules ars=apriori.getAssociationRules();
+		 for(AssociationRule ar:ars.getRules()){
+			 for(Item i:ar.getPremise()){
+				 System.out.println(i.getAttribute().index());/* important!  index of wordvec*/
+				 System.out.println(i.getAttribute().getRevision());
+				 System.out.println(i.getAttribute().name());/* important! name of feature namely:word */
+				 System.out.println(i.getAttribute().numValues());
+				 System.out.println(i.getAttribute().getClass());
+				 //System.out.print(i.getClass()+"\t"+i.getAttribute());
+				 System.out.println(i.toString());
+				 System.out.println(i.getComparisonAsString());
+				 System.out.println(i.getFrequency());
+				 System.out.println(i.getClass());
+				 System.out.println(i.getItemValueAsString());
 			 }
-			 System.out.println();
+			 System.out.println("------END");
 		 }
-
-		 System.out.println("-------------apriori-------------");
-		 System.out.println(apriori);
-		 System.out.println("-------------apriori-------------");
+		 
+//		 for(List list: listArray){
+//			 for(Object o: list){
+//				 System.out.print(o+"\t");
+//			 }
+//			 System.out.println();
+//		 }
+//
+//		 System.out.println("-------------apriori-------------");
+//		 System.out.println(apriori);
+//		 System.out.println("-------------apriori-------------");
 		 
 		 
 	}
